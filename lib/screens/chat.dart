c8c0612396e6c5405c9b56848d188294d75395ee
rohith <rohith.reddy@codetru.com>
2023-08-21 11:36:19 +0530

    completed the push notification

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,10 +1,34 @@
 import 'package:chat_notification/widgets/chat_message.dart';
 import 'package:chat_notification/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void setupPushNotification() async {
+    final fcm = FirebaseMessaging.instance;
+
+    await fcm.requestPermission();
+
+    // final token = await fcm.getToken();
+
+    // print('the token + ${token}');
+
+    fcm.subscribeToTopic("chat");
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupPushNotification();
+  }
 
   @override
   Widget build(BuildContext context) {
